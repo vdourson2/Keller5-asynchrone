38266a1f38194bd7a67f8359bb019844af2aa8c5
Virginie Dourson <virginiedourson@gmail.com>
2022-12-19 15:51:20 +0100

    Exercice 2 extra 1

--- Exercice 2/script.js
+++ Exercice 2/script.js
@@ -1,7 +1,23 @@
+function iso3166 (country){
+    switch (country){
+        case "belgium":
+            return "BE";
+            break;
+        case "france" :
+            return "FR";
+            break;
+        case "spain" :
+            return "ES";
+            break;
+        default :
+            console.log ("Sorry, this country isn't suported");
+    }
+}
 
 function agify(e) {
     let firstname = document.getElementById('firstname').value;
-    fetch("https://api.agify.io/?name=" + firstname)
+    let country = iso3166(document.getElementById('country').value)
+    fetch("https://api.agify.io/?name=" + firstname + "&country_id=" + country)
         .then((response) => response.text())
         .then((text) => {
             let newDiv = document.createElement('div');
@@ -9,7 +25,7 @@ function agify(e) {
             document.body.appendChild(newDiv);
         })
         .catch((error) => console.log("Error!"))
-    document.getElementById('firstname').value = "";
+    document.getElementById('formulaire').reset();
 }
 
 let button = document.querySelector('button');
